fbeef29936b1d5c815418d3afdcc6a0a72869f79
darkghost078 <daviddarkghost@gmail.com>
2025-11-02 18:03:47 +0100

    Feat: Background animation now has de parallax effect

--- src/app/components/background/background.tsx
+++ src/app/components/background/background.tsx
@@ -1,14 +1,38 @@
-import { motion } from "framer-motion";
+"use client"
+import { motion, useScroll } from "framer-motion";
+import { useEffect, useState } from "react";
 import styles from "./background.module.css";
 
 export default function BackgroundSticky() {
-  return (
-    <motion.div
-      className={styles.fondo}
-      initial={{ scale: 1 }}
-      animate={{ scale: 1.25 }}
-      transition={{ duration: 1.25}}
-      aria-hidden
-    />
-  );
+  const { scrollY } = useScroll();
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe = scrollY.on("change", (latest) => {
+      setScroll(latest);
+    });
+    return () => unsubscribe();
+  }, [scrollY]);
+
+  if (scroll < 400) {
+    return (
+      <motion.div
+        className={styles.fondo}
+        initial={{ scale: 1 }}
+        animate={{ scale: 1.25 }}
+        transition={{ duration: 1.75 }}
+        aria-hidden
+      />
+    );
+  } else {
+    return (
+      <motion.div
+        className={styles.fondo}
+        initial={{ scale: 1.25 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 1.75 }}
+        aria-hidden
+      />
+    );
+  }
 }
