407694f118f8363237376b62a46f894d2f4d16c0
David19 <davidsanchezpalomino200603@gmail.com>
2025-11-09 23:19:16 +0100

    Feat: footer v1

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -3,8 +3,27 @@ import styles from "./footer.module.css";
 
 export default function footer() {
   return (
-    <div>
-      <p className={styles.text}>This is the Footer</p>
+    <div className={styles.main}>
+      <div className={styles.footer}>
+
+        <div className={styles.info}>
+          <p className={styles.text}>Organiza:</p>
+          <Image src={"/logoAula.png"} alt="logoAula" width={236} height={236} />   {/** ¡¡¡ta mal el logo!!! */}
+          <div>
+            AQUI VAN LAS RRSS
+          </div>
+        </div>
+
+        <div className={styles.textContainer}>
+          <p className={styles.text}>
+            ¡Encuéntranos!
+            aulasoftwarelibre@uco.es
+             Aulario Averroes
+            (Campus de Rabanales)
+          </p>
+        </div>
+
+      </div>      
     </div>
   );
 }
