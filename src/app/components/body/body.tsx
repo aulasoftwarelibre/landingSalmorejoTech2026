76a721558e20e604b0f927826c942ca57baa3756
Zch13 <zchspain@gmi+ail.com>
2025-11-14 17:08:01 +0100

    body.tsx

--- src/app/components/body/body.tsx
+++ src/app/components/body/body.tsx
@@ -1,12 +1,34 @@
 import Image from "next/image";
+import logo from "/public/d37f886165abba69b34b989cc286df9138bc451b.png"
 import styles from "./body.module.css";
+import { style } from "framer-motion/client";
+
+const IMAGE_WIDTH = 1508;
+const IMAGE_HEIGHT = 241;
 
 export default function body() {
+
   return (
+    
+
     <div className={styles.fondo}>
+      <button className={styles.miboton}>
+        CALL4PAPER
+      </button>
+      <div className={styles.imagenConFiltro}>
+        <Image 
+          src="/d37f886165abba69b34b989cc286df9138bc451b.png" 
+          alt="Imagen con filtro de desenfoque" 
+          width={IMAGE_WIDTH} 
+          height={IMAGE_HEIGHT}
+          priority 
+        />
+      </div>
       <p className={styles.text}>
-      hola mundo
+      LA CITA DE LA TECNOLOGÍA CÓRDOBESA REGRESA CON UNA ENERGÍA IMPARABLE
 </p>
     </div>
   );
 }
+
+
