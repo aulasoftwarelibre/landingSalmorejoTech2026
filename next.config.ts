8e30d4507fe116197b82758d0ae10c20ea3e4f5b
David Martinez Molina <daviddarkghost@gmail.com>
2025-11-19 20:12:01 +0100

    Fix: Background route now is relative

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export'
+  basePath: '/2026'
 };
 
 export default nextConfig;
