6a3f11cd7fda0e7ea21ef85494647cc5362421bf
darkghost078 <daviddarkghost@gmail.com>
2025-11-19 11:49:25 +0100

    Feat: About Us done

---
+++ src/app/components/AboutUs/aboutUs.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import styles from "./aboutUs.module.css";
+import { style } from "framer-motion/client";
+
+const IMAGE_WIDTH = 1508;
+const IMAGE_HEIGHT = 241;
+
+export default function body() {
+
+  return (
+    <div className={styles.container}>
+      <h1><p className={styles.textAbout}>About Us</p></h1>
+      <p className={styles.text}>
+
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam scelerisque fringilla justo, ut elementum velit iaculis ac. Aenean egestas id arcu vitae scelerisque. In vehicula blandit venenatis. Nulla facilisi. In at massa metus. Maecenas commodo elementum ullamcorper. Quisque vestibulum malesuada maximus. Aliquam quis est eget justo vulputate placerat a et neque. Etiam sed venenatis dui. Integer placerat, tellus sed rhoncus facilisis, orci mi mollis tortor, vel suscipit urna nisi sit amet orci. Etiam erat leo, facilisis elementum lacinia bibendum, mollis vitae orci. Cras et accumsan tortor. Fusce tempor sit amet libero ut consectetur. Sed placerat fermentum cursus. Cras quis lobortis justo.
+
+Ut laoreet, odio vel ultricies vehicula, eros lectus pulvinar eros, sit amet pharetra neque mi ut nisi. Maecenas vehicula tellus ante, id suscipit ipsum ultricies sit amet. Etiam vestibulum ornare imperdiet. Maecenas sagittis molestie libero, nec ullamcorper erat maximus in. Nulla accumsan, velit a tempor accumsan, sapien purus semper libero, in venenatis neque odio ac est. Ut congue neque convallis tellus elementum, eu viverra mauris tempus. Integer et lacus ac mauris consectetur posuere.
+
+Suspendisse varius diam quis purus ornare, aliquet auctor magna sollicitudin. Mauris lobortis leo nec lorem bibendum sagittis. Aliquam varius lacus tortor, et ornare leo condimentum vel. Nam fermentum hendrerit purus vitae pulvinar. Mauris egestas vel mauris a dictum. Vivamus a lacus nec justo vulputate ultrices nec ac felis. Praesent pharetra massa lectus, nec porttitor enim condimentum vel. Curabitur eu feugiat ex, sit amet venenatis ex. Sed placerat diam sit amet quam convallis bibendum. Nam tempus tincidunt blandit. Sed at nisl erat. In vitae tincidunt odio, vel vulputate arcu. Nunc enim lorem, iaculis a venenatis ut, sollicitudin a metus. Nullam dictum nulla nec ligula vulputate, et luctus eros aliquet. Morbi id massa viverra, fermentum arcu nec, facilisis nunc.
+
+Ut eu mauris lobortis, mollis massa vel, posuere libero. Donec rutrum sit amet enim quis rhoncus. Vivamus consequat malesuada orci, non euismod nulla tincidunt a. Integer purus nisl, venenatis ut mattis sit amet, laoreet in nunc. Integer et pretium felis. Morbi dictum est vitae mauris posuere, vitae venenatis leo dignissim. Donec in mauris est. Curabitur nec ultricies ligula. Nullam molestie interdum risus vel finibus. Quisque commodo luctus leo, non rhoncus turpis egestas nec. Sed rhoncus dolor nec egestas vulputate. Mauris finibus, turpis sed dictum eleifend, ipsum dolor pharetra est, a pulvinar metus neque nec ante. Maecenas sed dapibus neque. Morbi efficitur libero et convallis efficitur.
+
+In tristique, quam vitae porttitor tempor, tortor massa venenatis erat, at posuere eros quam sit amet urna. Sed nibh ante, fringilla nec erat eget, hendrerit egestas elit. Curabitur tempus diam eu nibh fermentum, nec varius quam dapibus. In hac habitasse platea dictumst. Sed sit amet augue vel enim aliquet scelerisque sed ut justo. Pellentesque sollicitudin consectetur lacus, at pellentesque urna imperdiet id. Fusce scelerisque viverra luctus.
+
+      </p>
+    </div>
+  );
+}
+
+
