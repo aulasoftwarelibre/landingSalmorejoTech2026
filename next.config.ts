85d3eabe0a85a7bf201a2588b512d3d51db035dc
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-11-20 15:54:37 +0100

    fix: base path and unoptimized

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: '/2026landing',
-  output: 'export',
+  basePath: "",
+  output: "export",
   trailingSlash: true,
-
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
