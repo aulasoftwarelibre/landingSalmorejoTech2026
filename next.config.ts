421a89f2e123394717df216c149a3c221604644d
David Martinez Molina <daviddarkghost@gmail.com>
2025-11-20 16:04:35 +0100

    WHY IS NOT WORKING

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: "",
+  basePath: "/2026landing",
   output: "export",
   trailingSlash: true,
   images: {
