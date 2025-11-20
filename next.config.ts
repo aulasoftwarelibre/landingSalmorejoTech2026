47e2059a4b747f2369c00169337c34930d02cc15
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-11-20 10:15:44 +0100

    Fix: Images optimization

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: '/2026',
+  basePath: '/2026landing',
   output: 'export',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
