733f8977399b69a487eeef1bd4df9fde632eda73
David Martinez Molina <daviddarkghost@gmail.com>
2025-11-20 16:46:39 +0100

    Fix: Logo Salmorejo fixed pls work v2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: "/2026landing",
+  basePath: "",
   output: "export",
   trailingSlash: true,
   images: {
