42f9e4ba01e0699d4815b657d011e3263b0f6d7a
David Martinez Molina <daviddarkghost@gmail.com>
2025-11-20 18:53:30 +0100

    Fix: lets start again with the img

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: "/2026landing",
+  basePath: "/2026",
   output: "export",
   trailingSlash: true,
   images: {
